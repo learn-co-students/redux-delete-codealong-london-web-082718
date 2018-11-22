ae496b0a1b3cd19874598f78050f0a5672673c4e
braj011 <bhagyaraj01@gmail.com>
2018-11-22 01:08:53 +0000

    Done.

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,12 +1,27 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+
+  // OLD:
+  // switch (action.type) {
+  //   case 'ADD_TODO':
+  //     return { todos: state.todos.concat(action.payload.text) }
   switch (action.type) {
     case 'ADD_TODO':
-
-      return { todos: state.todos.concat(action.payload.text) };
+      const todo = {
+        id: Math.random()*10000000000000000,
+        text: action.payload.text
+      }
+      return { todos: state.todos.concat(todo) };
+    case 'DELETE_TODO':
+      return { todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
   }
 }
+
+//  Line 10 - 14
+// Using Math.random()*10000000000000000 will generate a long random number each time a todo is created. 
+// Now, instead of just storing an array of strings, we'll be storing an array of objects
+
